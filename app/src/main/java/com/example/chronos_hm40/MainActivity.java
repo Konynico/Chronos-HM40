ab63d1636a438f197b4cbf30261cc19d31f8267f
Konynico <thenicodu74@gmail.com>
2023-04-28 16:03:48 +0200

    V1 genre bien whoula

--- app/src/main/java/com/example/chronos_hm40/MainActivity.java
+++ app/src/main/java/com/example/chronos_hm40/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.chronos_hm40;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
 
+    public void onButtonClick(View view) {
+         Intent otherActivity = new Intent(getApplicationContext(), Menu.class);
+         startActivity(otherActivity);
+         finish();
+    }
 }
