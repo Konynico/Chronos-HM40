4bdad76d0ff0d28409085470671c493c72f46e59
EmilienRNN <renon.emilien@gmail.com>
2023-06-18 14:52:35 +0200

    maj quotidien

--- app/src/main/java/com/example/chronos_hm40/ScheduleActivity.java
+++ app/src/main/java/com/example/chronos_hm40/ScheduleActivity.java
@@ -119,7 +119,9 @@ public class ScheduleActivity extends AppCompatActivity {
 
                 if(course.getFrequency().equalsIgnoreCase("Quotidienne") && !currentDateWithoutTime.after(courseEndDate) && currentDateWithoutTime.after(courseStartDate)) {
                     courseList.add(course);
-                }else if(course.getDay().equalsIgnoreCase(selectedDay) && !currentDateWithoutTime.after(courseEndDate) && currentDateWithoutTime.after(courseStartDate)){
+                }else if(frequency.equalsIgnoreCase("Quotidienne") && course.getDay().equalsIgnoreCase(selectedDay) && !currentDateWithoutTime.after(courseEndDate)){
+                    courseList.add(course);
+                } else if(course.getDay().equalsIgnoreCase(selectedDay) && !currentDateWithoutTime.after(courseEndDate) && currentDateWithoutTime.after(courseStartDate)){
                     if(frequency.equalsIgnoreCase("Unique")){
                         courseList.add(course);
                     }else if(frequency.equalsIgnoreCase("Hebdomadaire") && daysBetween % 7 == 0){
